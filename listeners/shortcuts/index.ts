5d1da59807fe4639c174a538f3e16277b9dd62a9
yanis900 <yanis991@hotmail.com>
2025-12-22 19:19:47 +0000

    add shortcut: create jira ticket from thread callback function

--- listeners/shortcuts/index.ts
+++ listeners/shortcuts/index.ts
@@ -1,8 +1,10 @@
-import type { App } from '@slack/bolt';
-import { sampleShortcutCallback } from './sample-shortcut.js';
+import type { App } from "@slack/bolt";
+import { sampleShortcutCallback } from "./sample-shortcut.js";
+import { createJiraFromThreadAction } from "./create-jira-from-thread-action.js";
 
 const register = (app: App) => {
-  app.shortcut('sample_shortcut_id', sampleShortcutCallback);
+  app.shortcut("sample_shortcut_id", sampleShortcutCallback);
+  app.shortcut("create_jira_from_thread", createJiraFromThreadAction);
 };
 
 export default { register };
